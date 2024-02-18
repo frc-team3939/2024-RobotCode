10f9de01dae500731a53189fb1d2b443ae5f3464
MmmmmSpicy <43798691+MmmmmSpicy@users.noreply.github.com>
2024-02-18 15:11:23 -0500

    Command structure reformat

--- 2024-RobotCode/src/main/java/frc/robot/commands/intake/SpinIntake.java
+++ 2024-RobotCode/src/main/java/frc/robot/commands/intake/SpinIntake.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -31,7 +31,7 @@ public class SpinIntake extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    //intakeSubsystem.spinIntake(0);
+    intakeSubsystem.spinIntake(0);
     
   }
 
